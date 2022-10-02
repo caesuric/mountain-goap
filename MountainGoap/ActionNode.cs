5fcb326256255bc74bfbf0e9bfe00110b28aa8f0
caesuric <illusorycaesura@gmail.com>
2022-10-02 17:37:01 -0400

    added operator overrides for ActionGraph

--- MountainGoap/ActionNode.cs
+++ MountainGoap/ActionNode.cs
@@ -27,12 +27,52 @@ namespace MountainGoap {
             State = state;
         }
 
+        public static bool operator ==(ActionNode node1, ActionNode node2) {
+            if (node1 is null) return node2 is null;
+            if (node2 is null) return node1 is null;
+            if (node1.Action == null || node2.Action == null) return (node1.Action == node2.Action) && node1.StateMatches(node2);
+            return node1.Action.Equals(node2.Action) && node1.StateMatches(node2);
+        }
+
+        public static bool operator !=(ActionNode node1, ActionNode node2) {
+            if (node1 is null) {
+                return node2 is not null;
+            }
+            if (node2 is null) {
+                return node1 is not null;
+            }
+            return !node1.Action.Equals(node2.Action) || !node1.StateMatches(node2);
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object? o) {
+            if (o is not ActionNode item) return false;
+            return this == item;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode() {
+            return HashCode.Combine(Action, State);
+        }
+
         /// <summary>
         /// Cost to traverse this node.
         /// </summary>
-        /// <returns>THe cost of the action to be executed.</returns>
+        /// <returns>The cost of the action to be executed.</returns>
         internal float Cost() {
             return Action.Cost;
+        }
+
+        private bool StateMatches(ActionNode otherNode) {
+            foreach (var kvp in State) {
+                if (!otherNode.State.ContainsKey(kvp.Key)) return false;
+                if (!otherNode.State[kvp.Key].Equals(kvp.Value)) return false;
+            }
+            foreach (var kvp in otherNode.State) {
+                if (!State.ContainsKey(kvp.Key)) return false;
+                if (!State[kvp.Key].Equals(kvp.Value)) return false;
+            }
+            return true;
         }
     }
 }
