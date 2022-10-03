d900624a9d7c0db603a24eafc443b4159d3b1b32
caesuric <illusorycaesura@gmail.com>
2022-10-03 10:49:22 -0400

    renamed misnamed variable in examples

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -15,12 +15,12 @@
         /// <param name="args">Arguments to the application.</param>
         /// <returns>An exit code.</returns>
         public static async Task<int> Main(string[] args) {
-            var demo1Command = new Command("happiness", "Run the happiness incrementer demo.");
-            demo1Command.SetHandler(() => {
+            var happinessIncrementerCommand = new Command("happiness", "Run the happiness incrementer demo.");
+            happinessIncrementerCommand.SetHandler(() => {
                 RunHappinessIncrementer();
             });
             var cmd = new RootCommand {
-                demo1Command
+                happinessIncrementerCommand
             };
             return await cmd.InvokeAsync(args);
         }
