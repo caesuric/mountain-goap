179a37e3ded8a641c779549e6cfb08da8be75c8b
Jan Werder <jan@werder.space>
2023-12-14 14:10:16 +0100

    Create ArithmeticPostconditionsTests.cs

---
+++ MountainGoapTest/ArithmeticPostconditionsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MountainGoapTest
+{
+    public class ArithmeticPostconditionsTests
+    {
+        [Fact]
+        public void MinimalExampleText()
+        {
+
+            List<BaseGoal> goals = new List<BaseGoal>(){
+                new Goal(
+                    name: "Goal1",
+                    desiredState: new() {
+                        { "i", new ComparisonValuePair {
+                            Value = 100,
+                            Operator = ComparisonOperator.GreaterThan
+                        } }
+                    },
+                    weight: 1f
+                ),
+            };
+
+            List<MountainGoap.Action> actions = new List<MountainGoap.Action>(){
+                new MountainGoap.Action(
+                    name: "Action1",
+                    executor: (Agent agent, MountainGoap.Action action) => {
+                        return ExecutionStatus.Succeeded;
+                    },
+                    arithmeticPostconditions: new Dictionary<string, object> {
+                        { "i", 10 }
+                    },
+                    cost: 0.5f
+                ),
+            };
+
+            Agent agent = new Agent(
+                goals: goals,
+                actions: actions,
+                state: new() {
+                    { "i", 0 }
+                }
+            );
+
+            agent.Step(StepMode.OneAction);
+            Assert.Equal(10, agent.State["i"]);
+
+        }
+    }
+}
